aa368c35b13583f155916909eca84f4f6a643bb3
SI <>
2021-11-25 11:47:12 +0900

    HTMLからJavaScriptを読み込む位置を移動

--- docs/javaScript.js
+++ docs/javaScript.js
@@ -6,7 +6,7 @@
 const scroll_to_top_btn = document.querySelector('#scroll-to-top-btn');
 
 //クリックイベントを追加
-scroll_to_top_btn.addEventListener( 'click' , scroll_to_top );
+scroll_to_top_btn.addEventListener('click', scroll_to_top);
 
 function scroll_to_top(){
   window.scroll({top: 0, behavior: 'smooth'});
